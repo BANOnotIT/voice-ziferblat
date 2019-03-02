7a28e7f1075c8af44b1eecef8226fac4bfe2ec18
BANO.notIT <bano.notit@yandex.ru>
2019-03-03 00:49:24 +0300

    core: Использовать gpiozero.LED и ООП, вместо императива

---
+++ digit_controller.py
@@ -0,0 +1,47 @@
+from gpiozero import LED
+
+"""
+ =0=
+|   |
+1   2
+|   |
+ =3=
+|   |
+4   5
+|   |
+ =6=
+"""
+
+MAP_VAL = {
+    0: [0, 1, 2, 4, 5, 6],
+    1: [2, 5],
+    2: [0, 2, 3, 4, 6],
+    3: [0, 2, 3, 5, 6],
+    4: [1, 2, 3, 5],
+    5: [0, 1, 3, 5, 6],
+    6: [0, 1, 3, 4, 5, 6],
+    7: [0, 2, 5],
+    8: [0, 1, 2, 3, 4, 5, 6],
+    9: [0, 1, 2, 3, 5, 6]
+}
+MAP_ADDR = {
+    0: 16,
+    1: 20,
+    2: 21,
+    3: 6,
+    4: 13,
+    5: 19,
+    6: 26,
+}
+
+
+class DigitOutputController:
+    def __init__(self, masks=MAP_VAL, addresses=MAP_ADDR):
+        self._leds = [LED(i) for i in addresses.values()]
+        self._masks = masks
+
+    def show(self, digit=0):
+        vals = self._masks.get(digit, [])
+
+        for i, led in enumerate(self._leds):
+            led.value = i in vals
