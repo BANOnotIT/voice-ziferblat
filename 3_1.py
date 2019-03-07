a880ed2397f9c229a54c907674ac4ce7bdb81c7e
BANO.notIT <bano.notit@yandex.ru>
2019-03-07 14:27:08 +0300

    task: Реализовать испытание 3.1

---
+++ 3_1.py
@@ -0,0 +1,33 @@
+from core import detector
+from core.digit_controller import DigitOutputController
+from settings import PA_AUDIO_DEVICE_NAME
+
+controller = DigitOutputController()
+
+print("Initializing audio recognizer... Wait")
+stream = detector.get_words_stream(PA_AUDIO_DEVICE_NAME)
+print("Initialized! You can speak now")
+
+mapping = {
+    'ноль': 0,
+    'один': 1,
+    'два': 2,
+    'три': 3,
+    'четыре': 4,
+    'пять': 5,
+    'шесть': 6,
+    'семь': 7,
+    'восемь': 8,
+    'девять': 9,
+}
+
+for word in stream:
+    print(word)
+
+    if word in 'стоп сброс':
+        print("Stopping loop")
+        break
+
+    controller.show(mapping[word])
+
+print("Exit")
