734eef4e227b7f37903ae215a1a42209f36ddab0
BANO.notIT <bano.notit@yandex.ru>
2019-03-07 16:26:29 +0300

    fix: Использовать основной поток для распознавания речи

--- 3_2.py
+++ 3_2.py
@@ -15,56 +15,55 @@ queue = Queue()
 
 
 ###################################
-def listener():
-    for word in stream:
-        queue.put(word)
 
+
+def worker():
+    """
+    Всего есть 3 состояния:
+    0 - ожидаем команды на запуск
+     - - "один"
+    1 - показываем циферки
+     - - "стоп"
+    2 - ожидаем команду "сброс"
+     - - "сброс"
+     --- выход из цикла
+    """
+    stage = 0
+
+    euler_digits_queue = read(path.join(dirname, 'nums_data/euler'))
+
+    while True:
+        if stage == 0:
+            word = queue.get()
+            if word == 'заводи':
+                stage = 1
+                print("")
+            queue.task_done()
+
+        elif stage == 1:
+            word = None if queue.empty() else queue.get_nowait()
+            if word == 'стоп':
+                stage = 2
+                continue
+
+            controller.show(next(euler_digits_queue))
+            sleep(1)
+
+        else:
+            word = None if queue.empty() else queue.get_nowait()
+            if word == 'сброс':
+                controller.show(0)
+                break
+
+
+threading.Thread(target=worker).start()
 
 ###################################
-print("Initializing audio recognizer... Wait")
 
+print("Инициализация словоря...")
 stream = detector.get_words_stream(PA_AUDIO_DEVICE_NAME)
-thread = threading.Thread(target=listener)
+print("Можно говорить!")
 
-print("Initialized! You can speak now")
-###################################
-
-"""
-Всего есть 3 состояния:
-0 - ожидаем команды на запуск
- - - "один"
-1 - показываем циферки
- - - "стоп"
-2 - ожидаем команду "сброс"
- - - "сброс"
- --- выход из цикла
-"""
-
-stage = 0
-
-euler_digits_queue = read(path.join(dirname, 'nums_data/euler'))
-
-while True:
-    if stage == 0:
-        word = queue.get()
-        if word == 'один':
-            stage = 1
-        queue.task_done()
-
-    elif stage == 1:
-        word = None if queue.empty() else queue.get_nowait()
-        if word == 'стоп':
-            stage = 2
-            continue
-
-        controller.show(next(euler_digits_queue))
-        sleep(1)
-
-    else:
-        word = None if queue.empty() else queue.get_nowait()
-        if word == 'сброс':
-            controller.show(0)
-            break
-
-print("Program stopped. Enter smth to exit")
-input()
+for word in stream:
+    print("  < ", word)
+    queue.put_nowait(str(word))
